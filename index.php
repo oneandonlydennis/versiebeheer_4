1dcad26074065e6129af02d59a76fd7162373147
Dennis <dennisluijkenaar@gmail.com>
2019-05-29 14:37:03 +0200

    Forgot an ID at index.php! Click the h1 to check

--- index.php
+++ index.php
@@ -41,7 +41,7 @@
   </head>
   <body>
       <h1 onclick="showtext()">Dit is een leuke nieuwe website met erg veel content ! :)</h1>
-      <h2 class="hidden">maar max is fucking gay</h2>
+      <h2 class="hidden" id="hidden">maar max is fucking gay</h2>
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
